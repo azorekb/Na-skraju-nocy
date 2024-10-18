22dc907d7b420f98a56e5ab8537980a02647a4c0
LAPTOP-94A87LBS\azore <azorekb@gmail.com>
2024-10-18 18:34:42 +0200

    Creating GitHub repository

---
+++ register.php
@@ -0,0 +1,91 @@
+<html>
+<?php
+session_start();
+$connected = false;
+$mysql = mysql_connect('mysql1.ugu.pl','db699059','MalaRuka.037');
+if($mysql)
+{
+    if(mysql_select_db('db699059', $mysql))
+    {
+        $connected = true;
+    }
+    else echo 'can\'t select db';
+}
+else echo 'Could not connect to database';
+
+if($connected)
+{
+    if(isset($_GET['what']))
+    {
+        switch($_GET['what'])
+        {
+            case 'new':
+            {
+                if(isset($_POST['username']) && isset($_POST['password']) && isset($_POST['mail']))
+                {
+                    $result = mysql_query("select username,mail from nsn_login where username = '" . $_POST['username'] . "' or mail = '" . $_POST['mail'] . "';", $mysql);
+                    if($result)
+                    {
+                        $array = mysql_fetch_array($result);
+                        if($array[0] == $_POST['username']) header('location: index.php?error=1');
+                        if($array[1] == $_POST['mail']) header('location: index.php?error=2');
+                        
+                        if(!$array)
+                        {
+                            $kod = '';
+                            for($i = 0; $i < 8; $i++)
+                            $kod .= rand(0,9);
+                        
+                            $to = $_POST['mail'];
+                            $subject = "Na skraju nocy - wizja";
+                            
+                            $message = "<img src='http://naskrajunocy.ugu.pl/img/scroll.png' style='margin-bottom:-400px; width: 340px;height: 360px'><div style=\"width: 340px;height: 360px;color: darkblue;font-family: MV Boli;text-align: center;display: flex;\"><p style=\"display: inline-block;align-self: center;padding: 0 65px;\">{$_POST['username']}<br><br>W nocy ukazały ci się przedziwne znaki:<br><br>{$kod}<br><br>[link będzie później]</p></div>";
+                            
+                            $headers = "MIME-Version: 1.0\r\n";
+                            $headers .= "Content-type: text/html; charset=UTF-8\r\n";
+                            $headers .= 'From: <szibi@naskrajunocy.ugu.pl>';
+                            
+                            mail($to,$subject,$message,$headers);
+                            
+                            mysql_query("insert into nsn_login(username,password,mail,code) values ('{$_POST['username']}','{$_POST['password']}','{$_POST['mail']}','{$kod}');", $mysql) or die(mysql_error());
+                            
+                            $_SESSION["username"] = $_POST["username"];
+                            $_SESSION["mail"] = $_POST["mail"];
+                            
+                            header("location: index.php?action=registered");
+                        }
+                        
+                    }
+                    else echo 'error: ' . mysql_error();
+                }
+                else header("location: index.php");
+            } break;
+            case "confirm":
+            {
+                if(isset($_POST["vision"]))
+                {
+                    $result = mysql_query("select code from nsn_login where username = '" . $_SESSION['username'] . "';", $mysql);
+
+                    if($result)
+                    {
+                        $array = mysql_fetch_array($result);
+                        if($array[0] == $_POST["vision"])
+                        {
+                            mysql_query("update nsn_login set confirmed = 1 where username = '" . $_SESSION['username'] . "';", $mysql);
+                            header("location:  index.php?confmed=true");
+                        }
+                        else header("location:  index.php?action=registered&error=3");
+                    }
+                }
+                else header("location: index.php");
+            } break;
+    
+            default: header("location: index.php");
+        }
+    }
+    else header("location: index.php");
+}
+
+?>
+
+</html>
